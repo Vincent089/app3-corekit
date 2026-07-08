488105a2df8d038ee65ed16c7115258dddacc9ae
Vincent <vincent.corriveau89@gmail.com>
2026-07-08 09:17:21 -0400

    utilis db mixin

---
+++ src/corekit/db/mixin.py
@@ -0,0 +1,26 @@
+#   -----------------------------------------------------------------------------
+#  Copyright (c) 2026. Vincent Corriveau (vincent.corriveau89@gmail.com)
+#
+#  Licensed under the MIT License. You may obtain a copy of the License at
+#  https://opensource.org/licenses/MIT
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  -----------------------------------------------------------------------------
+from datetime import datetime, timezone
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
